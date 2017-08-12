60afd9266e3dbc361aeac57df35a8f7dc754c395
George Lewis <glewis@iqt.org>
2017-08-12 16:39:37 -0400

    init

---
+++ 30daysofcode/day1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "strconv"
+)
+
+func main() {
+    var i uint64 = 4
+    var d float64 = 4.0
+    var s string = "HackerRank "
+
+    scanner := bufio.NewScanner(os.Stdin)
+
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+}
